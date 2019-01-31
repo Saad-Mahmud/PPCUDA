c06b8c566ffcdecb7582594b99c05b57dd64d44e
Saad-Mahmud <saadmahmud14@gmail.com>
2019-02-01 01:58:17 +0600

    Update MatMul.cu

--- Part-3/MatMul.cu
+++ Part-3/MatMul.cu
@@ -1,17 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Kernel function to add the elements of two arrays
+// Kernel function for matrix multiplication
 __global__
 void GPUmatmul(int N, double *x, double *y, double *ans)
 {
+	//calculates unique thread ID in the block
 	int t= (blockDim.x*blockDim.y)*threadIdx.z+(threadIdx.y*blockDim.x)+(threadIdx.x);
-    	int b= (gridDim.x*gridDim.y)*blockIdx.z+(blockIdx.y*gridDim.x)+(blockIdx.x);
-    	int T= blockDim.x*blockDim.y*blockDim.z;
-    	int B= gridDim.x*gridDim.y*gridDim.z;
-    
-    	for (int i=b;i<N;i+=B)
-    	{
+	//calculates unique block ID in the grid
+	int b= (gridDim.x*gridDim.y)*blockIdx.z+(blockIdx.y*gridDim.x)+(blockIdx.x);
+	//block size (this is redundant though)
+	int T= blockDim.x*blockDim.y*blockDim.z;
+	//grid size (this is redundant though)
+	int B= gridDim.x*gridDim.y*gridDim.z;
+	
+	/*
+	 * Each cell in the matrix is assigned to a different thread. 
+	 * Each thread do O(N*number of asssigned cell) computation.
+	 * Assigned cells of different threads does not overlape with
+	 * each other. And so no need for synchronization.
+	 */
+	 
+    for (int i=b;i<N;i+=B)
+    {
 		for(int j=t;j<N;j+=T)
 		{
 			for(int k=0;k<N;k++)
@@ -36,7 +47,7 @@ void CPUmatmul(int N,double *x, double *y, double *ans)
 	}
 }
 
-bool sanity_check(int N,double *ans)
+bool check(int N,double *ans)
 {
 	for(int i=0;i<N;i++)
 	{
@@ -48,21 +59,12 @@ bool sanity_check(int N,double *ans)
 	return true;
 }
 
-void printtt(int N,double *ans)
-{
-	for(int i=0;i<16;i++)
-	{
-		for(int j=0;j<16;j++)
-		{
-			cout<<ans[i*N+j]<<" ";
-		}
-		cout<<endl;
-	}
-}
+
 int main(void)
 {
-
-	int N = 1<<13;
+	//size of matrix
+	int N = 1<<9;
+	
 	double *x, *y, *ans;
 
 	// Allocate Unified Memory – accessible from CPU or GPU
@@ -81,25 +83,25 @@ int main(void)
 		}
 	}
 
-  
-  clock_t t;
-  double avg=0;
-  // Run kernel on 1M elements on the GPU
-  for(int i=0;i<=3;i++)
-  {
-	  t=clock();
-	  //CPUmatmul(N, x, y,ans);
-	  t = clock() - t;
-	  if(i)avg+=t;
-	  printf ("It took CPU-%d %f ms.\n",i,(((double)t)/CLOCKS_PER_SEC)*1000);
-  }
-  avg/=3;
-  avg/=CLOCKS_PER_SEC;
-  avg*=1000;
-  printf ("It took %lf ms on avg.\n",avg);
-  if(sanity_check(N,ans))cout<<"RUN OK."<<endl;
-  else cout<<"RUN NOT OK."<<endl;
- 
+
+	clock_t t;
+	double avg=0;
+	cout<<"Strting CPU computation"<<endl;
+	for(int i=0;i<=3;i++)
+	{
+		t=clock();
+		CPUmatmul(N, x, y,ans);
+		t = clock() - t;
+		if(i)avg+=t;  //we will ignore the first run
+		printf ("It took CPU-%d %f ms.\n",i,(((double)t)/CLOCKS_PER_SEC)*1000);
+	}
+	avg/=3;
+	avg/=CLOCKS_PER_SEC;
+	avg*=1000;
+	printf ("It took %lf ms on avg.\n",avg);
+	if(check(N,ans))cout<<"RUN OK."<<endl;
+	else cout<<"RUN NOT OK."<<endl;
+
 	// initialize x,y and ans arrays on the host
 	for (int i = 0; i < N; i++) 
 	{
@@ -110,30 +112,28 @@ int main(void)
 			ans[i*N+j]=(double)0.000000000000;
 		}
 	}
-   avg=0;
-  // Run kernel on 1M elements on the GPU
-  for(int i=0;i<=3;i++)
-  {
-	  t=clock();
-	  GPUmatmul<<<dim3(16,16,16), dim3(16,8,8)>>>(N, x, y,ans);
-	  cudaDeviceSynchronize();
-	  t = clock() - t;
-	  if(i)avg+=t;
-	  printf ("It took GPU-%d %f ms.\n",i,(((double)t)/CLOCKS_PER_SEC)*1000);
-  }
-  avg/=3;
-  avg/=CLOCKS_PER_SEC;
-  avg*=1000;
-  printf ("It took %lf ms on avg.\n",avg);
-  if(sanity_check(N,ans))cout<<"RUN OK."<<endl;
-  else cout<<"RUN NOT OK."<<endl;
+	avg=0;
+	cout<<"Strting GPU computation"<<endl;
+	// Run kernel on GPU
+	for(int i=0;i<=3;i++)
+	{
+		t=clock();
+		GPUmatmul<<<dim3(16,16,16), dim3(16,8,8)>>>(N, x, y,ans);
+		cudaDeviceSynchronize();
+		t = clock() - t;
+		if(i)avg+=t; //we will ignore the first run
+		printf ("It took GPU-%d %f ms.\n",i,(((double)t)/CLOCKS_PER_SEC)*1000);
+	}
+	avg/=3;
+	avg/=CLOCKS_PER_SEC;
+	avg*=1000;
+	printf ("It took %lf ms on avg.\n",avg);
+	if(check(N,ans))cout<<"RUN OK."<<endl;
+	else cout<<"RUN NOT OK."<<endl;
 
-  
-  // Free memory
-  cudaFree(x);
-  cudaFree(y); 
-  return 0;
+
+	// Free memory
+	cudaFree(x);
+	cudaFree(y); 
+	return 0;
 }
-
-
-
